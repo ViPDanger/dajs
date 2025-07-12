689ecbda468b8d33c085c565fd0da3f4cb3ac315
= <vip.danger@mail.ru>
2025-07-13 01:18:37 +0300

    изменено:      .gitignore 	изменено:      telegram-bot/main.py

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,12 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from options import BOT_TOKEN
 from handlers import routers
 
 
-API_TOKEN = '7620989653:AAE8eiBN0MvCTnjip8lMRjbOAHTeyZRlMoo'
 
 async def main():
-    bot = Bot(token=API_TOKEN)
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
     for router in routers:
         dp.include_router(router)
